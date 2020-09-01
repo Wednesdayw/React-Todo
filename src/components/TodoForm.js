ce6b07af291e22fe61923c7f00556182863eabab
Melissa C <66797764+Wednesdayw@users.noreply.github.com>
2020-09-01 09:57:31 -0400

    updates and styling

---
+++ src/components/TodoForm.js
@@ -0,0 +1,113 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+//styling using styled-components
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    input {
+      border: none;
+      border-bottom: thin solid blue;
+      text-align: center;
+    }
+    button {
+      border: none;
+      background: none;
+      margin-top: 1.5rem;
+      border: thin solid blue;
+      padding: 0.25rem ${(0.25 * 16) / 9}rem;
+      border-radius: 0.25rem;
+      transition: 0.125s ease-in-out all;
+      margin: 1.5rem 0.5rem 0rem 0.5rem;
+      &:hover {
+        background: blue;
+        color: white;
+        transition: 0.125s ease-in-out all;
+        cursor: pointer;
+      }
+      &:disabled {
+        cursor: not-allowed;
+        color: grey;
+        &:hover {
+          background: none;
+          color: grey;
+        }
+      }
+    }
+  }
+`;
+
+class TodoForm extends Component {
+  //constructor with state
+  constructor(props) {
+    super(props);
+    this.state = {
+      itemText: "",
+      allowSubmit: false,
+    };
+  }
+
+  handleChange = (e) => {
+    //update state with each keystroke
+    const { name, value } = e.target;
+    this.setState({ ...this.state, [name]: value });
+  };
+  //class property to submit form
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.itemText, () => {
+      this.setState({ ...this.state, itemText: "" });
+    });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (!prevState.allowSubmit) {
+      if (this.state.itemText.length > 0) {
+        this.setState({ ...this.state, allowSubmit: true });
+      }
+    } else {
+      if (this.state.itemText.length <= 0) {
+        this.setState({ ...this.state, allowSubmit: false });
+      }
+    }
+  }
+
+  clearCompleted = (e) => {
+    const { onClear } = this.props;
+    e.preventDefault();
+    onClear();
+  };
+
+  render() {
+    return (
+      <Container>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            name="itemText"
+            id="itemText"
+            onChange={this.handleChange}
+            value={this.state.itemText}
+            placeholder="New Item"
+          />
+          <div>
+            <button disabled={!this.state.allowSubmit} type="submit">
+              Add Item
+            </button>
+            <button onClick={this.clearCompleted}>Clear Completed</button>
+          </div>
+        </form>
+      </Container>
+    );
+  }
+}
+
+export default TodoForm;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,31 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React, { Component } from "react";
+import styled from "styled-components";
+import TodoItem from "./Todo";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  border-top: thin dashed black;
+  margin-top: 1rem;
+`;
+
+class TodoList extends Component {
+  render() {
+    const { todoList, toggleCompleted } = this.props;
+    return (
+      <Container>
+        <h2>To Do List</h2>
+        {todoList.map((item) => {
+          return <TodoItem item={item} key={item.id} toggleCompleted={toggleCompleted}/>;
+        })}
+      </Container>
+    );
+  }
+}
+
+export default TodoList;
